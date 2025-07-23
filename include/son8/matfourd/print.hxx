9ea27e1362dc10818e82fbce672662c78f471874
Oleg'Ease'Kharchuk <olegease@gmail.com>
2025-07-23 08:53:07 +0300

    patch-2 (#2)

--- include/son8/matfourd/print.hxx
+++ include/son8/matfourd/print.hxx
@@ -2,19 +2,33 @@
 #define SON8_MATFOURD_PRINT_HXX
 
 #include "core.hxx"
+#include "layout.hxx"
 #include "vec.hxx"
+#include "mat.hxx"
 
 #include <iostream>
 
 namespace son8::matfourd {
     // Vec (column vector) print function
     template< typename Type, unsigned Size, bool Layt >
-    SON8_MATFOURD_FUNC operator<<( std::ostream &os, Vec< Type, Size, Layt > const &vec ) -> std::ostream & {
+    SON8_MATFOURD_DISC operator<<( std::ostream &os, Vec< Type, Size, Layt > const &vec ) -> std::ostream & {
         if constexpr ( Layt ) os << "~";
         os << "Vec" << Size << "([" << vec.x( ) << ", " << vec.y( );
         if constexpr ( Size > 2 ) { os << ", " << vec.z( ); }
         if constexpr ( Size > 3 ) { os << ", " << vec.w( ); }
         os << "])";
+        return os;
+    }
+    // Mat (column matrix) print function
+    template< typename Type, unsigned Rows, unsigned Cols, bool Layt >
+    SON8_MATFOURD_DISC operator<<( std::ostream &os, Mat< Type, Rows, Cols, Layt > const &mat ) -> std::ostream & {
+        if constexpr ( Layt ) os << "~";
+        os << "Mat" << Rows << "x" << Cols << "([";
+        os << "\n\t" << mat.v1( ) << ",\n\t" << mat.v2( );
+
+        if constexpr ( mat.vecs( ) > 2 ) os << ",\n\t" << mat.v3( );
+        if constexpr ( mat.vecs( ) > 3 ) os << ",\n\t" << mat.v4( );
+        os << "\n])";
         return os;
     }
 }
