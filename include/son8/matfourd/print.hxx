3934033cbebbdbc9d36e26b147b4f10d9f1b450a
Oleg'Ease'Kharchuk <olegease@gmail.com>
2025-07-29 03:47:46 +0300

    output Vec and Mat improve

--- include/son8/matfourd/print.hxx
+++ include/son8/matfourd/print.hxx
@@ -13,23 +13,24 @@ namespace son8::matfourd {
     template< typename Type, unsigned Size, bool Layt >
     SON8_MATFOURD_DISC operator<<( std::ostream &os, Vec< Type, Size, Layt > const &vec ) -> std::ostream & {
         if constexpr ( Layt ) os << "~";
-        os << "Vec" << Size << "([" << vec.x( ) << ", " << vec.y( );
+        os << "Vec" << Size << "[" << vec.x( ) << ", " << vec.y( );
         if constexpr ( Size > 2 ) { os << ", " << vec.z( ); }
         if constexpr ( Size > 3 ) { os << ", " << vec.w( ); }
-        os << "])";
+        os << "]";
         return os;
     }
     // Mat (column matrix) print function
     template< typename Type, unsigned Rows, unsigned Cols, bool Layt >
     SON8_MATFOURD_DISC operator<<( std::ostream &os, Mat< Type, Rows, Cols, Layt > const &mat ) -> std::ostream & {
         using SelfType = Mat< Type, Rows, Cols, Layt >;
+        os << "\n";
         if constexpr ( Layt ) os << "~";
-        os << "Mat" << Rows << "x" << Cols << "([";
+        os << "Mat" << Rows << "x" << Cols << "{";
         os << "\n\t" << mat.v1( ) << ",\n\t" << mat.v2( );
 
         if constexpr ( SelfType::vecs( ) > 2 ) os << ",\n\t" << mat.v3( );
         if constexpr ( SelfType::vecs( ) > 3 ) os << ",\n\t" << mat.v4( );
-        os << "\n])";
+        os << "\n}";
         return os;
     }
 }
