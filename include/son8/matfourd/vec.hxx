e3a91a321ca9765c0b2c73f8f3faecbd8df34073
Oleg'Ease'Kharchuk <olegease@gmail.com>
2025-12-10 03:47:24 +0200

    Swizzles 2025 12 09 (#14)
    * opengl-like swizzles impl
    
    * swizzle vars macros and global include

--- include/son8/matfourd/vec.hxx
+++ include/son8/matfourd/vec.hxx
@@ -5,6 +5,7 @@
 #include <son8/matfourd/vec/alias.hxx>
 #include <son8/matfourd/vec/equality.hxx>
 #include <son8/matfourd/vec/multiply.hxx>
+#include <son8/matfourd/vec/swizzles.hxx>
 #include <son8/matfourd/vec/type.hxx>
 
 #endif//SON8_MATFOURD_VEC_HXX
