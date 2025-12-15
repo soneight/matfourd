cef3ea0203eb96b5002a19de0e348e4810aee18e
Oleg'Ease'Kharchuk <olegease@gmail.com>
2025-12-15 02:48:10 +0200

    add col and row aliases for vec and mat

--- include/son8/matfourd/mat/alias.hxx
+++ include/son8/matfourd/mat/alias.hxx
@@ -5,7 +5,7 @@
 #include <son8/matfourd/mat/type.hxx>
 
 namespace son8::matfourd {
-    // Mat (column matrix) aliases
+    // default Mat (column matrix) aliases
     template< typename Type, bool Layt = Layout::ColMajor > using Mat2   = Mat< Type, 2, 2, Layt >;
     template< typename Type, bool Layt = Layout::ColMajor > using Mat3   = Mat< Type, 3, 3, Layt >;
     template< typename Type, bool Layt = Layout::ColMajor > using Mat4   = Mat< Type, 4, 4, Layt >;
@@ -15,7 +15,26 @@ namespace son8::matfourd {
     template< typename Type, bool Layt = Layout::ColMajor > using Mat3x4 = Mat< Type, 3, 4, Layt >;
     template< typename Type, bool Layt = Layout::ColMajor > using Mat4x2 = Mat< Type, 4, 2, Layt >;
     template< typename Type, bool Layt = Layout::ColMajor > using Mat4x3 = Mat< Type, 4, 3, Layt >;
-
+    // col (column-major) aliases
+    template< typename Type, bool Layt = Layout::ColMajor > using Col2x2 = Mat< Type, 2, 2, Layt >;
+    template< typename Type, bool Layt = Layout::ColMajor > using Col3x3 = Mat< Type, 3, 3, Layt >;
+    template< typename Type, bool Layt = Layout::ColMajor > using Col4x4 = Mat< Type, 4, 4, Layt >;
+    template< typename Type, bool Layt = Layout::ColMajor > using Col2x3 = Mat< Type, 2, 3, Layt >;
+    template< typename Type, bool Layt = Layout::ColMajor > using Col2x4 = Mat< Type, 2, 4, Layt >;
+    template< typename Type, bool Layt = Layout::ColMajor > using Col3x2 = Mat< Type, 3, 2, Layt >;
+    template< typename Type, bool Layt = Layout::ColMajor > using Col3x4 = Mat< Type, 3, 4, Layt >;
+    template< typename Type, bool Layt = Layout::ColMajor > using Col4x2 = Mat< Type, 4, 2, Layt >;
+    template< typename Type, bool Layt = Layout::ColMajor > using Col4x3 = Mat< Type, 4, 3, Layt >;
+    // row (row-major) aliases
+    template< typename Type, bool Layt = Layout::RowMajor > using Row2x2 = Mat< Type, 2, 2, Layt >;
+    template< typename Type, bool Layt = Layout::RowMajor > using Row3x3 = Mat< Type, 3, 3, Layt >;
+    template< typename Type, bool Layt = Layout::RowMajor > using Row4x4 = Mat< Type, 4, 4, Layt >;
+    template< typename Type, bool Layt = Layout::RowMajor > using Row2x3 = Mat< Type, 2, 3, Layt >;
+    template< typename Type, bool Layt = Layout::RowMajor > using Row2x4 = Mat< Type, 2, 4, Layt >;
+    template< typename Type, bool Layt = Layout::RowMajor > using Row3x2 = Mat< Type, 3, 2, Layt >;
+    template< typename Type, bool Layt = Layout::RowMajor > using Row3x4 = Mat< Type, 3, 4, Layt >;
+    template< typename Type, bool Layt = Layout::RowMajor > using Row4x2 = Mat< Type, 4, 2, Layt >;
+    template< typename Type, bool Layt = Layout::RowMajor > using Row4x3 = Mat< Type, 4, 3, Layt >;
 } // namespace son8::matfourd
 
 #endif//SON8_MATFOURD_MAT_ALIAS_HXX
--- include/son8/matfourd/vec/alias.hxx
+++ include/son8/matfourd/vec/alias.hxx
@@ -5,10 +5,18 @@
 #include <son8/matfourd/vec/type.hxx>
 
 namespace son8::matfourd {
-    // Vec (column vector) aliases
+    // default Vec (column vector) aliases
     template< typename Type, bool Layt = Layout::ColMajor > using Vec2 = Vec< Type, 2, Layt >;
     template< typename Type, bool Layt = Layout::ColMajor > using Vec3 = Vec< Type, 3, Layt >;
     template< typename Type, bool Layt = Layout::ColMajor > using Vec4 = Vec< Type, 4, Layt >;
+    // col (column-major) aliases
+    template< typename Type, bool Layt = Layout::ColMajor > using Col2 = Vec< Type, 2, Layt >;
+    template< typename Type, bool Layt = Layout::ColMajor > using Col3 = Vec< Type, 3, Layt >;
+    template< typename Type, bool Layt = Layout::ColMajor > using Col4 = Vec< Type, 4, Layt >;
+    // row (row-major) aliases
+    template< typename Type, bool Layt = Layout::RowMajor > using Row2 = Vec< Type, 2, Layt >;
+    template< typename Type, bool Layt = Layout::RowMajor > using Row3 = Vec< Type, 3, Layt >;
+    template< typename Type, bool Layt = Layout::RowMajor > using Row4 = Vec< Type, 4, Layt >;
 
 } // namespace son8::matfourd
 
