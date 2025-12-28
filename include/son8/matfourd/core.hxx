8b8fcf3d76ce2c7c1305ea309c62461470846c54
Oleg'Ease'Kharchuk <olegease@gmail.com>
2025-12-28 10:43:08 +0200

    forsaken add core alias

--- include/son8/matfourd/core.hxx
+++ include/son8/matfourd/core.hxx
@@ -1,8 +1,15 @@
 #ifndef SON8_MATFOURD_CORE_HXX
 #define SON8_MATFOURD_CORE_HXX
+// std
+#include <initializer_list>
 
 #define SON8_MATFOURD_FUNC [[nodiscard]] inline auto
 #define SON8_MATFOURD_DISC inline auto
+
+namespace son8::matfourd {
+    template< typename Type >
+    using ForsakenInitialist_ = std::initializer_list< std::initializer_list< Type > >;
+}
 
 #endif//SON8_MATFOURD_CORE_HXX
 
