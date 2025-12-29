7ebbc0f8a7be118f4d1b05ce3ea5c9059af5e4f5
Oleg'Ease'Kharchuk <olegease@gmail.com>
2025-12-29 17:55:36 +0200

    update deprecated layout header

--- include/son8/matfourd/layout.hxx
+++ include/son8/matfourd/layout.hxx
@@ -1,22 +1,11 @@
 #ifndef SON8_MATFOURD_LAYOUT_HXX
 #define SON8_MATFOURD_LAYOUT_HXX
 
-namespace son8::matfourd {
+#ifndef SON8_MATFOURD_INCLUDE_DEPRECATED
+#error "son8::matfourd: layout is subject to be removed/changed use <son8/matfourd/order.hxx> instead or define (or set cmake option) SON8_MATFOURD_INCLUDE_DEPRECATED"
+#endif
 
-    class Layout {
-        struct RowMajor_ {
-            constexpr operator bool( ) const { return true; }
-        };
-
-        struct ColMajor_ {
-            constexpr operator bool( ) const { return false; }
-        };
-    public:
-        static constexpr RowMajor_ RowMajor{ };
-        static constexpr ColMajor_ ColMajor{ };
-    };
-
-}
+#include <son8/matfourd/order.hxx>
 
 #endif//SON8_MATFOURD_LAYOUT_HXX
 
